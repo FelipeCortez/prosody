2931f26b505cdf3c7a412c6e4a356119acffa1b0
Felipe Cortez <felipecortezfi@gmail.com>
2018-04-13 18:16:58 -0300

    Conversão básica funcionando

--- src/sampa-mbrola.py
+++ src/sampa-mbrola.py
@@ -1,12 +1,43 @@
-def load_sampa_mbrola():
-    equivs = {}
-    with open("sampa-mbrola.phonemes") as f:
-        for line in f:
-            k, v = line.split()
-            equivs[k] = v
-            print(k, v)
-    return equivs
+import sys
 
+class Converter():
+    def __init__(self):
+        self.load_sampa_mbrola()
+
+    def load_sampa_mbrola(self):
+        equivs = {}
+        with open("sampa-mbrola.phonemes") as f:
+            for line in f:
+                v, k = line.split()
+                equivs[k] = v
+
+        self.equivs = equivs
+
+    def convert_phoneme(self, phoneme: str):
+        if self.equivs and phoneme in self.equivs:
+            return self.equivs[phoneme]
+        else:
+            return phoneme
+
+    def convert_sentence(self, input_str):
+        result = ["_"]
+        forbidden = [
+            "\n",
+            ",",
+            "'"
+        ]
+
+        input_str = input_str.strip()
+
+        for phoneme in input_str:
+            if phoneme not in forbidden:
+                result.append(self.convert_phoneme(phoneme))
+
+        result.append("_")
+        return "\n".join(result)
 
 if __name__ == "__main__":
-    load_sampa_mbrola()
+    converter = Converter()
+    for line in sys.stdin:
+        print(line)
+        print(converter.convert_sentence(line))
