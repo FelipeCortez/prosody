ea312b7fd4aa119b1a5bc57206c0f0a172e42a8b
Felipe Cortez <felipecortezfi@gmail.com>
2018-04-13 17:35:01 -0300

    Diagrama atualizado, conversão inicial SAMPA-MBROLA

--- src/sampa-mbrola.py
+++ src/sampa-mbrola.py
@@ -1,2 +1,12 @@
+def load_sampa_mbrola():
+    equivs = {}
+    with open("sampa-mbrola.phonemes") as f:
+        for line in f:
+            k, v = line.split()
+            equivs[k] = v
+            print(k, v)
+    return equivs
+
+
 if __name__ == "__main__":
-    print("Convert")
+    load_sampa_mbrola()
