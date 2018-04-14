23ec73c0e169313443832d3e6dc1598022d50d5c
Felipe Cortez <felipecortezfi@gmail.com>
2018-04-14 07:26:17 -0300

    Exemplos adicionados à tabela de conversão SAMPA-MBROLA, durações
    Durações do LianeTTS

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# Geração de prosódia para sistemas TTS
+
+## Créditos
+- Vozes sintetizadas usando MBROLA com a voz `br3`, por Denis R. Costa
+- Duração de fonemas em `durations.tbl` do [LianeTTS]
--- src/sampa-mbrola.py
+++ src/sampa-mbrola.py
@@ -3,21 +3,37 @@ import sys
 class Converter():
     def __init__(self):
         self.load_sampa_mbrola()
+        self.load_durations()
 
     def load_sampa_mbrola(self):
         equivs = {}
-        with open("sampa-mbrola.phonemes") as f:
+        with open("sampa-mbrola.tbl") as f:
             for line in f:
-                v, k = line.split()
+                v, k, _ = line.split()
                 equivs[k] = v
 
         self.equivs = equivs
+
+    def load_durations(self):
+        durations = {}
+        with open("durations.tbl") as f:
+            for line in f:
+                k, v = line.split()
+                durations[k] = v
+
+        self.durations = durations
 
     def convert_phoneme(self, phoneme: str):
         if self.equivs and phoneme in self.equivs:
             return self.equivs[phoneme]
         else:
             return phoneme
+
+    def get_duration(self, phoneme: str) -> int:
+        if self.durations and phoneme in self.durations:
+            return self.durations[phoneme]
+        else:
+            return 100
 
     def convert_sentence(self, input_str):
         result = ["_"]
@@ -31,7 +47,9 @@ class Converter():
 
         for phoneme in input_str:
             if phoneme not in forbidden:
-                result.append(self.convert_phoneme(phoneme))
+                mbrola_line = "{} {}".format(self.convert_phoneme(phoneme),
+                                             str(self.get_duration(phoneme)))
+                result.append(mbrola_line)
 
         result.append("_")
         return "\n".join(result)
