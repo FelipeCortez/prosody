46b3ca479f84ad3ee924324aa9a3bc8ab76dec84
Felipe Cortez <felipecortezfi@gmail.com>
2018-04-17 20:49:59 -0300

    Pitch constante para sintetizar voz com MBROLA

--- src/sampa-mbrola.py
+++ src/sampa-mbrola.py
@@ -1,5 +1,6 @@
 import sys
 
+
 class Converter():
     def __init__(self):
         self.load_sampa_mbrola()
@@ -24,6 +25,10 @@ class Converter():
         self.durations = durations
 
     def convert_phoneme(self, phoneme: str):
+        # não é _tão_ simples assim
+        # fazer lookahead com regex
+        # regras am, em, etc etc
+        # s pode ser s (antes de vogal) ou s2 (antes de consoante)
         if self.equivs and phoneme in self.equivs:
             return self.equivs[phoneme]
         else:
@@ -36,26 +41,28 @@ class Converter():
             return 100
 
     def convert_sentence(self, input_str):
-        result = ["_"]
-        forbidden = [
-            "\n",
-            ",",
-            "'"
-        ]
+        result = ["_ 50 50 150"]
+        forbidden = ["\n", ",", "'"]
 
         input_str = input_str.strip()
 
         for phoneme in input_str:
             if phoneme not in forbidden:
-                mbrola_line = "{} {}".format(self.convert_phoneme(phoneme),
-                                             str(self.get_duration(phoneme)))
+                mbrola_line = "{} {} {} {}".format(
+                    self.convert_phoneme(phoneme),
+                    str(self.get_duration(phoneme)),
+                    "50",
+                    "150"
+                )
+
                 result.append(mbrola_line)
 
-        result.append("_")
+        result.append("_ 50 50 150")
         return "\n".join(result)
+
 
 if __name__ == "__main__":
     converter = Converter()
     for line in sys.stdin:
-        print(line)
+        # print(line)
         print(converter.convert_sentence(line))
