aca45a7ed309376d5b61ae2b5a4c53b9f75a9a6b
Felipe Cortez <felipecortezfi@gmail.com>
2018-04-22 17:58:03 -0300

    Testes corrigidos

--- src/tests.py
+++ src/tests.py
@@ -7,13 +7,13 @@ class TestConversions(unittest.TestCase):
         self.converter = Converter()
 
     def testPhonemeConversion(self):
-        self.assertEqual(self.converter.convert_phoneme("u~bigo"), "um")
-        self.assertEqual(self.converter.convert_phoneme("sala"), "s")
-        #self.assertEqual(self.converter.convert_phoneme("casca"), "s")
+        self.assertEqual(self.converter.convert_phoneme("u~bigo")[0], "um")
+        self.assertEqual(self.converter.convert_phoneme("sala")[0], "s")
+        # casca
 
 
 class TestMisc(unittest.TestCase):
-    def testFlatten():
+    def testFlatten(self):
         self.assertEqual(flatten([[50, 150], [70, 130]]),
                          [50, 150, 70, 130])
 
