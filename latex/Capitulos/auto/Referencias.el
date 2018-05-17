46facf8fe01cecfabd96a5cff26b6c3d04403dbd
Felipe Cortez <felipecortezfi@gmail.com>
2018-05-17 15:21:23 -0300

    Bibliografia da proposta, comentários com conteúdo de capítulos

---
+++ latex/Capitulos/auto/Referencias.el
@@ -0,0 +1,18 @@
+(TeX-add-style-hook
+ "Referencias"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "siri"
+    "cortana"
+    "alexa"
+    "bick"
+    "PaquetePhD"
+    "Jaszkiewicz"
+    "KnowlesBoundedPareto"
+    "KnowlesBoundedLebesgue"
+    "LopezPaqueteStu"
+    "Silberschatz"
+    "DB2XML"
+    "Angelo"))
+ :bibtex)
+
