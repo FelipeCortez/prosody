ce23b1289cbcc07bd11d1ef5909282d7e4426a9c
Felipe Cortez <felipecortezfi@gmail.com>
2018-06-15 21:17:31 -0300

    Algoritmo INTSINT em Python

---
+++ src/editor/api/intsint.py
@@ -0,0 +1,29 @@
+from math import sqrt
+
+def intsint(labels: list, key: float = 220.0, range: float = 1.0):
+    if labels[0] not in ["T", "M", "B"]:
+        raise ValueError("First item in list can't be relative")
+
+    freq_t = key * sqrt(2 ** range)
+    freq_m = key
+    freq_b = key / sqrt(2 ** range)
+
+    freqs = []
+
+    for idx, l in enumerate(labels):
+        if l == "T":
+            freqs.append(freq_t)
+        elif l == "M":
+            freqs.append(freq_m)
+        elif l == "B":
+            freqs.append(freq_b)
+        elif l == "H":
+            freqs.append(sqrt(labels[idx - 1]) * freq_t)
+        elif l == "S":
+            freqs.append(labels[idx - 1])
+        elif l == "L":
+            freqs.append(sqrt(labels[idx - 1]) * freq_b)
+        elif l == "U":
+            freqs.append(sqrt(labels[idx - 1]) * sqrt(labels[idx - 1] * freq_t))
+        elif l == "D":
+            freqs.append(sqrt(labels[idx - 1]) * sqrt(labels[idx - 1] * freq_b))
