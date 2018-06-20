4d857d4d63e856505a3c6716a5d5bd2fa1fe579e
Felipe Cortez <felipecortezfi@gmail.com>
2018-06-20 00:00:43 -0300

    Slides! Com imagens! Chegou o grande dia!

--- slides/auto/slides.el
+++ slides/auto/slides.el
@@ -4,12 +4,12 @@
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("babel" "brazilian")))
    (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -17,11 +17,22 @@
     "beamer10"
     "inputenc"
     "babel"
+    "algorithm"
+    "algorithmic"
     "amsmath"
     "amsfonts"
     "amssymb"
+    "tikz"
     "times")
+   (TeX-add-symbols
+    "signed")
    (LaTeX-add-labels
-    "fig:tts-arch"))
+    "fig:tts-arch"
+    "fig:arch"
+    "tab:intsint")
+   (LaTeX-add-environments
+    '("aquote" 1))
+   (LaTeX-add-saveboxes
+    "mybox"))
  :latex)
 
